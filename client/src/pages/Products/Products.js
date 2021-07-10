780471ff3e5e8531b193f45a3eb3d61ea34614bf
ignaciosein@gmail.com <ignaciosein@gmail.com>
2021-07-10 17:12:32 +0200

    pagina products ordena de mayor a menor los productos

--- client/src/pages/Products/Products.js
+++ client/src/pages/Products/Products.js
@@ -6,26 +6,65 @@ const Products = () => {
 
   useEffect(() => {
     axios("/api/nacho").then((resultado) => {
+      /*    let arrayAsc = resultado.data.sort((a,b) => a.Price - b.Price)
+
+      console.log(arrayAsc)
+ */
       setProducts(resultado.data);
     });
   }, []);
+
+  /*     console.log(products) */
+
+  //PRECIO ASCENDENTE
+  /*  console.log(products.sort((a,b) => a.Price - b.Price));   */
+  //PRECIO DESCENDENTE
+  /*  console.log(products.sort((a,b) => b.Price - a.Price));    */
+
+  const priceAsc = () => {
+
+    const sorted = [...products].sort((a, b) => {
+      return a.Price - b.Price;
+    });
+
+    setProducts(sorted)
+
+  /*   setProducts(products.sort((a, b) => b.Price - a.Price)); */
+
+
+
+  };
+
+  const priceDesc = () => {
+
+
+    const sorted = [...products].sort((a, b) => {
+      return b.Price - a.Price;
+    });
+
+    setProducts(sorted)
+
+
+  };
+  
+
 
   console.log(products);
 
   return (
     <div>
       LISTADO DE PRODUCTOS:
+      <div>
+        <button onClick={priceAsc}>PRECIO ASCENDENTE</button>
+        <button onClick={priceDesc}>PRECIO ASCENDENTE</button>
+      </div>
       <ul>
         {products.map((item, i) => (
-          <li key={i}>{item.Name}</li>
+          <li key={i}>
+            {item.Name} Precio: {item.Price} Relevancia {item.Relevance}{" "}
+          </li>
         ))}
 
-        {products.map((item, i) => (
-          <li key={i}>{item.Price}</li>
-        ))}
-        {products.map((item, i) => (
-          <li key={i}>{item.Relevance}</li>
-        ))}
         {/*      <li>Nombre {products.Name}</li>
 
         <li>Precio {products.Price}</li>
